79ba9755a4ebf2b059fdd0632b417afce6c14a5e
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2023-10-10 11:55:53 -0500

    ass

---
+++ src/utils/range.ts
@@ -0,0 +1,16 @@
+export interface IRange {
+	min: number;
+	max: number;
+	isInRange(number: number): boolean;
+}
+export class Range implements IRange {
+	min: number;
+	max: number;
+	constructor(min: number, max: number) {
+		this.min = min;
+		this.max = max;
+	}
+	isInRange(number: number): boolean {
+		return number >= this.min && number <= this.max;
+	}
+}
