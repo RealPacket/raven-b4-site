45448575a6497b42b71680b6339ec9a67ec01a7b
betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>
2024-01-16 18:56:43 -0600

    fix a skill issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,5 @@
-// set this in production to "/raven-b4-site"
-export const BASE_PATH = "/raven-b4-site";
+// set this in production to "/raven-b4-site/"
+export const BASE_PATH = "/raven-b4-site/";
+if (!BASE_PATH.endsWith("/")) {
+	throw new Error("base path is malformed!");
+}
